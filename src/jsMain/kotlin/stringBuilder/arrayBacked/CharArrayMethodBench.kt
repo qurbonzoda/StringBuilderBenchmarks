6706188f87347d32177bb7ea4761bf35359aeef6
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2019-10-21 11:42:25 +0300

    Add append CharArray benchmarks for CharArrayStringBuilder

--- src/jsMain/kotlin/stringBuilder/arrayBacked/CharArrayMethodBench.kt
+++ src/jsMain/kotlin/stringBuilder/arrayBacked/CharArrayMethodBench.kt
@@ -9,13 +9,15 @@ class CharArrayMethodBench {
     @Param("1", "10", "100", "1000", "10000", "100000")
     var size = 0
 
-    var string = ""
-    var charArray = charArrayOf()
+    private var string = ""
+    private var charArrayToAppend = charArrayOf()
+    private var charArray = charArrayOf()
 
     @Setup
     fun setup() {
         string = ""
         repeat(size) { string += Random.nextInt().toChar() }
+        charArrayToAppend = CharArray(size) { Random.nextInt().toChar() }
 
         charArray = CharArray(size) { Random.nextInt().toChar() }
     }
@@ -38,6 +40,27 @@ class CharArrayMethodBench {
     @Benchmark
     fun appendString_toCharArray(): CharArray {
         string.toCharArray().copyInto(charArray, 0, 0, string.length)
+        return charArray
+    }
+
+    @Benchmark
+    fun appendCharArray_CharIn(): CharArray {
+        var length = 0
+        for (char in charArrayToAppend) charArray[length++] = char
+        return charArray
+    }
+
+    @Benchmark
+    fun appendCharArray_CharByIndex(): CharArray {
+        var length = 0
+        for (index in 0 until charArrayToAppend.size) charArray[length++] = charArrayToAppend[index]
+        return charArray
+    }
+
+    @UseExperimental(ExperimentalStdlibApi::class)
+    @Benchmark
+    fun appendCharArray_CopyInto(): CharArray {
+        charArrayToAppend.copyInto(charArray, 0, 0, charArrayToAppend.size)
         return charArray
     }
 }
